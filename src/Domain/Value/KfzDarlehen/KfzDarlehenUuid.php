cbc8ea46884c7d4828cdeb144029a902cf508bc6
Oskar Stark <oskarstark@googlemail.com>
2020-04-22 23:14:17 +0200

    Transfered lib

--- src/Domain/Value/KfzDarlehen/KfzDarlehenUuid.php
+++ src/Domain/Value/KfzDarlehen/KfzDarlehenUuid.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of LF-Api.
+ *
+ * (c) Oskar Stark <oskarstark@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Gansel\LF\Api\Domain\Value\KfzDarlehen;
 
 use Ramsey\Uuid\Uuid;
